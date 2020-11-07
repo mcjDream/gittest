de78758c3204b2d9301eb6db0cdd1db93c8f8ec9
mcj <97911@qq.com>
2020-11-07 14:25:45 +0800

    添加两条sout

---
+++ ssh/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+        System.out.println("Hello git World!");
+    }
+}
